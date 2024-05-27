e934db55e8b82b1d582835c78aa26a8c71d1250f
forhappiness12s12 <danielchen12s12@gmail.com>
2024-05-27 08:08:21 -0700

    5th commit

--- src/Components/Joggers/SkinnyFitStyle.js
+++ src/Components/Joggers/SkinnyFitStyle.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { StyleContext } from "./StyleProviderJoggers";
-import { useContext,startTransition } from "react";
+import { useContext, startTransition } from "react";
 import { Tooltip } from 'react-tooltip'
 import halfpointedright from './stylepic/half-pointed-right.png'
 import halfpointedleft from './stylepic/half-pointed-left.png'
@@ -36,136 +36,73 @@ export const SkinnyFitStyle = () => {
     return (
         <div className=" bg-[#141414]  border-solid rounded-md border-2  border-[#bbb5b5] p-2 mx-2 shadow-[#faf397] shadow-lg">
             <article className="text-white flex items-center justify-center text-[18px] font-bold">Skiny Fit Leg Style Option</article>
-            <div className="grid grid-cols-3 grid-row-1 p-1">
-                    <div className="text-white text-[13px]" >ROPE</div>
-                    <label className="container text-white" data-tooltip-id="my-tooltip-left" data-tooltip-place="top">
+            <div className="grid grid-cols-2 grid-row-5 p-1 flex items-center">
+                <div className="text-white text-[20px] p-2" >WITH ROPE</div>
+                <label className="container text-white p-2" data-tooltip-id="my-tooltip-left" data-tooltip-place="top">
 
-                        <input type="radio" name="rope" onChange={() => Change_Model_Index_call(5)} />
-                        <span className="checkmark px-1"></span>
-                        TRUE
-                    </label>
-                    <Tooltip id="my-tooltip-left" className='opacity-100 justify-center items-center z-10' >
-                        <div>
-                            <img src={halfpointedright} alt="Cat" className='w-[100px]' />
-                            <span >T32K2</span>
-                        </div>
-                    </Tooltip>
-                    <label
-                        className="container text-white"
-                        data-tooltip-id="my-tooltip-right"
-                        data-tooltip-place="top"
-                    >
+                    <input type="radio" name="rope" onChange={() => Change_Model_Index_call(5)} />
+                    <span className="checkmark px-1"></span>
 
-                        <input type="radio" name="rope" onChange={() => Change_Model_Index_call(6)} />
-                        <span className="checkmark px-1"></span>
-                        FAlSE
-                    </label>
-                    <Tooltip id="my-tooltip-right" className="opacity-100 z-10">
-                        <div>
-                            <img src={halfpointedleft} alt="Cat" className='w-[100px]' />
-                            <span>T32K3</span>
-                        </div>
-                    </Tooltip>
+                </label>
+                <Tooltip id="my-tooltip-left" className='opacity-100 justify-center items-center z-10' >
+                    <div>
+                        <img src={halfpointedright} alt="Cat" className='w-[100px]' />
+                        <span >T32K2</span>
+                    </div>
+                </Tooltip>
+                <div className="text-white text-[20px] p-2" >RIB CUFF</div>
+                <label
+                    className="container text-white p-2"
+                    data-tooltip-id="my-tooltip-right"
+                    data-tooltip-place="top"
+                >
 
-                </div>
-                {/* fly */}
-                <div className="grid grid-cols-3 grid-row-1 p-1">
-                    <div className="text-white text-[13px]" >FLY</div>
-                    <label className="container text-white" data-tooltip-id="my-tooltip-left" data-tooltip-place="top">
+                    <input type="radio" name="rope" onChange={() => Change_Model_Index_call(6)} />
+                    <span className="checkmark px-1"></span>
 
-                        <input type="radio" name="rope" onChange={() => Change_Model_Index_call(7)} />
-                        <span className="checkmark px-1"></span>
-                        TRUE
-                    </label>
-                    <Tooltip id="my-tooltip-left" className='opacity-100 justify-center items-center' >
-                        <div>
-                            <img src={halfpointedright} alt="Cat" className='w-[100px]' />
-                            <span >T32K2</span>
-                        </div>
-                    </Tooltip>
-                    <label
-                        className="container text-white"
-                        data-tooltip-id="my-tooltip-right"
-                        data-tooltip-place="top"
-                    >
+                </label>
+                <Tooltip id="my-tooltip-right" className="opacity-100 z-10">
+                    <div>
+                        <img src={halfpointedleft} alt="Cat" className='w-[100px]' />
+                        <span>T32K3</span>
+                    </div>
+                </Tooltip>
+                <div className="text-white text-[20px] p-2" >FRONT POCKET</div>
+                <label className="container text-white p-2" data-tooltip-id="my-tooltip-left" data-tooltip-place="top">
 
-                        <input type="radio" name="rope" onChange={() => Change_Model_Index_call(8)} />
-                        <span className="checkmark px-1"></span>
-                        FAlSE
-                    </label>
-                    <Tooltip id="my-tooltip-right" className="opacity-100">
-                        <div>
-                            <img src={halfpointedleft} alt="Cat" className='w-[100px]' />
-                            <span>T32K3</span>
-                        </div>
-                    </Tooltip>
+                    <input type="radio" name="rope" onChange={() => Change_Model_Index_call(7)} />
+                    <span className="checkmark px-1"></span>
 
-                </div>
-                {/* Hem */}
-                <div className="grid grid-cols-3 grid-row-1 p-1">
-                    <div className="text-white text-[13px]" >HEM</div>
-                    <label className="container text-white" data-tooltip-id="my-tooltip-left" data-tooltip-place="top">
+                </label>
+                <Tooltip id="my-tooltip-left" className='opacity-100 justify-center items-center' >
+                    <div>
+                        <img src={halfpointedright} alt="Cat" className='w-[100px]' />
+                        <span >T32K2</span>
+                    </div>
+                </Tooltip>
+                <div className="text-white text-[20px] p-2" >GENERAL</div>
+                <label
+                    className="container text-white p-2"
+                    data-tooltip-id="my-tooltip-right"
+                    data-tooltip-place="top"
+                >
 
-                        <input type="radio" name="rope" onChange={() => Change_Model_Index_call(9)} />
-                        <span className="checkmark px-1"></span>
-                        TRUE
-                    </label>
-                    <Tooltip id="my-tooltip-left" className='opacity-100 justify-center items-center' >
-                        <div>
-                            <img src={halfpointedright} alt="Cat" className='w-[100px]' />
-                            <span >T32K2</span>
-                        </div>
-                    </Tooltip>
-                    <label
-                        className="container text-white"
-                        data-tooltip-id="my-tooltip-right"
-                        data-tooltip-place="top"
-                    >
+                    <input type="radio" name="rope" onChange={() => Change_Model_Index_call(8)} />
+                    <span className="checkmark px-1"></span>
+                    
+                </label>
+                <Tooltip id="my-tooltip-right" className="opacity-100">
+                    <div>
+                        <img src={halfpointedleft} alt="Cat" className='w-[100px]' />
+                        <span>T32K3</span>
+                    </div>
+                </Tooltip>
 
-                        <input type="radio" name="hem" onChange={() => BackPocketStyleChange(1)} />
-                        <span className="checkmark px-1"></span>
-                        FAlSE
-                    </label>
-                    <Tooltip id="my-tooltip-right" className="opacity-100">
-                        <div>
-                            <img src={halfpointedleft} alt="Cat" className='w-[100px]' />
-                            <span>T32K3</span>
-                        </div>
-                    </Tooltip>
+            </div>
 
-                </div>
-                <div className="grid grid-cols-3 grid-row-1 p-1">
-                    <div className="text-white text-[13px]" >HEM</div>
-                    <label className="container text-white" data-tooltip-id="my-tooltip-left" data-tooltip-place="top">
 
-                        <input type="radio" name="flap" onChange={() => BackPocketStyleChange(0)} />
-                        <span className="checkmark px-1"></span>
-                        TRUE
-                    </label>
-                    <Tooltip id="my-tooltip-left" className='opacity-100 justify-center items-center' >
-                        <div>
-                            <img src={halfpointedright} alt="Cat" className='w-[100px]' />
-                            <span >T32K2</span>
-                        </div>
-                    </Tooltip>
-                    <label
-                        className="container text-white"
-                        data-tooltip-id="my-tooltip-right"
-                        data-tooltip-place="top"
-                    >
 
-                        <input type="radio" name="flap" onChange={() => BackPocketStyleChange(1)} />
-                        <span className="checkmark px-1"></span>
-                        FAlSE
-                    </label>
-                    <Tooltip id="my-tooltip-right" className="opacity-100">
-                        <div>
-                            <img src={halfpointedleft} alt="Cat" className='w-[100px]' />
-                            <span>T32K3</span>
-                        </div>
-                    </Tooltip>
 
-                </div>
         </div>
 
     );
